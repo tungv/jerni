45d39f26455a2732bc4c5662acf5f4e1f75ff2d0
Tung Vu <me@tungv.com>
2018-07-20 11:31:31 +0700

    test: add spec for errors

--- packages/cli/tests/cli.test.js
+++ packages/cli/tests/cli.test.js
@@ -13,3 +13,23 @@ test('should start with less than 300ms', async t => {
   const output = await start({ args: ['--port=8081'], timeoutMs: 0 });
   t.true(Date.now() - startTime < 300);
 });
+
+test('should report if redis cannot be connected', async t => {
+  const error = await t.throws(
+    start({
+      args: ['--port=8082', '--redis=redis://localhost:9999'],
+    })
+  );
+
+  t.true(error.message.includes('Redis connection to localhost:9999 failed'));
+});
+
+test('should report if port cannot be opened', async t => {
+  const error = await t.throws(
+    start({
+      args: ['--port=80'],
+    })
+  );
+
+  t.true(error.message.includes('Error: listen EACCES 0.0.0.0:80'));
+});
