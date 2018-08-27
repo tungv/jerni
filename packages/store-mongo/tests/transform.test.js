0f70433247c62e639ad6e962eac89ece94482c7b
Tung Vu <me@tungv.com>
2018-08-27 17:57:04 +0700

    test(mongo): update optimistic spec

--- packages/store-mongo/tests/transform.test.js
+++ packages/store-mongo/tests/transform.test.js
@@ -1,39 +1,39 @@
-const kefir = require('kefir');
-const test = require('ava');
+const kefir = require("kefir");
+const test = require("ava");
 
-const transform = require('../lib/transform');
+const transform = require("../lib/transform");
 
 const childrenBorn = () => ({
   id: 10,
-  type: 'CHILDREN_BORN',
+  type: "CHILDREN_BORN",
   payload: {
     children: [
       {
-        id: '11',
-        name: 'child_1',
+        id: "11",
+        name: "child_1"
       },
       {
-        id: '12',
-        name: 'child_2',
+        id: "12",
+        name: "child_2"
       },
       {
-        id: '13',
-        name: 'child_3',
-      },
+        id: "13",
+        name: "child_3"
+      }
     ],
-    parents: ['1', '2'],
+    parents: ["1", "2"]
   },
   meta: {
-    occurred_at: 1533632235302,
-  },
+    occurred_at: 1533632235302
+  }
 });
 
-test('transform', t => {
+test("transform", t => {
   const ops = transform(sampleChildrenBornEventHandler, childrenBorn());
   t.snapshot(ops);
 });
 
-test('transform: updateMany', t => {
+test("transform: updateMany", t => {
   const ops = transform(
     event => [
       {
@@ -41,12 +41,12 @@ test('transform: updateMany', t => {
           where: { x: 2 },
           changes: {
             $set: { x: 1 },
-            $inc: { y: 1 },
-          },
-        },
-      },
+            $inc: { y: 1 }
+          }
+        }
+      }
     ],
-    { id: 2, type: 'test' }
+    { id: 2, type: "test" }
   );
 
   t.deepEqual(ops, [
@@ -56,85 +56,85 @@ test('transform: updateMany', t => {
           $and: [
             { x: 2 },
             {
-              $or: [{ __v: { $lt: 2 } }, { __v: 2, __op: { $lt: 0 } }],
-            },
-          ],
+              $or: [{ __v: { $lt: 2 } }, { __v: 2, __op: { $lt: 0 } }]
+            }
+          ]
         },
         update: {
           $set: {
             __v: 2,
             __op: 0,
-            x: 1,
+            x: 1
           },
-          $inc: { y: 1 },
+          $inc: { y: 1 }
         },
-        upsert: false,
-      },
-    },
+        upsert: false
+      }
+    }
   ]);
 });
 
-test('transform: insertMany', t => {
+test("transform: insertMany", t => {
   const ops = transform(
     event => [
       {
         insertMany: [
-          { id: 1, name: 'doc_1' },
-          { id: 2, name: 'doc_2' },
-          { id: 3, name: 'doc_3' },
-        ],
-      },
+          { id: 1, name: "doc_1" },
+          { id: 2, name: "doc_2" },
+          { id: 3, name: "doc_3" }
+        ]
+      }
     ],
-    { id: 2, type: 'test' }
+    { id: 2, type: "test" }
   );
 
   t.deepEqual(ops, [
     {
       updateOne: {
-        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gt: 0 } }] },
-        update: { $setOnInsert: { id: 1, name: 'doc_1', __v: 2, __op: 0 } },
-        upsert: true,
-      },
+        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gte: 0 } }] },
+        update: { $setOnInsert: { id: 1, name: "doc_1", __v: 2, __op: 0 } },
+        upsert: true
+      }
     },
     {
       updateOne: {
-        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gt: 1 } }] },
-        update: { $setOnInsert: { id: 2, name: 'doc_2', __v: 2, __op: 1 } },
-        upsert: true,
-      },
+        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gte: 1 } }] },
+        update: { $setOnInsert: { id: 2, name: "doc_2", __v: 2, __op: 1 } },
+        upsert: true
+      }
     },
     {
       updateOne: {
-        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gt: 2 } }] },
-        update: { $setOnInsert: { id: 3, name: 'doc_3', __v: 2, __op: 2 } },
-        upsert: true,
-      },
-    },
+        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gte: 2 } }] },
+        update: { $setOnInsert: { id: 3, name: "doc_3", __v: 2, __op: 2 } },
+        upsert: true
+      }
+    }
   ]);
 });
 
-test('transform: insertOne', t => {
+test("transform: insertOne", t => {
   const ops = transform(
     event => [
       {
-        insertOne: { id: 1, name: 'doc_1' },
-      },
+        insertOne: { id: 1, name: "doc_1" }
+      }
     ],
-    { id: 2, type: 'test' }
+    { id: 2, type: "test" }
   );
 
   t.deepEqual(ops, [
     {
       updateOne: {
-        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gt: 0 } }] },
-        update: { $setOnInsert: { id: 1, name: 'doc_1', __v: 2, __op: 0 } },
-        upsert: true,
-      },
-    },
+        filter: { $or: [{ __v: { $gt: 2 } }, { __v: 2, __op: { $gte: 0 } }] },
+        update: { $setOnInsert: { id: 1, name: "doc_1", __v: 2, __op: 0 } },
+        upsert: true
+      }
+    }
   ]);
 });
 
-test('transform: updateMany', t => {
+test("transform: updateMany", t => {
   const ops = transform(
     event => [
       {
@@ -142,12 +142,12 @@ test('transform: updateMany', t => {
           where: { x: 1 },
           changes: {
             $set: { x: 2 },
-            $inc: { y: 1 },
-          },
-        },
-      },
+            $inc: { y: 1 }
+          }
+        }
+      }
     ],
-    { id: 2, type: 'test' }
+    { id: 2, type: "test" }
   );
 
   t.deepEqual(ops, [
@@ -156,20 +156,20 @@ test('transform: updateMany', t => {
         filter: {
           $and: [
             { x: 1 },
-            { $or: [{ __v: { $lt: 2 } }, { __v: 2, __op: { $lt: 0 } }] },
-          ],
+            { $or: [{ __v: { $lt: 2 } }, { __v: 2, __op: { $lt: 0 } }] }
+          ]
         },
         update: {
           $set: { x: 2, __v: 2, __op: 0 },
-          $inc: { y: 1 },
+          $inc: { y: 1 }
         },
-        upsert: false,
-      },
-    },
+        upsert: false
+      }
+    }
   ]);
 });
 
-test('transform: updateOne', t => {
+test("transform: updateOne", t => {
   const ops = transform(
     event => [
       {
@@ -177,12 +177,12 @@ test('transform: updateOne', t => {
           where: { x: 1 },
           changes: {
             $set: { x: 2 },
-            $inc: { y: 1 },
-          },
-        },
-      },
+            $inc: { y: 1 }
+          }
+        }
+      }
     ],
-    { id: 2, type: 'test' }
+    { id: 2, type: "test" }
   );
 
   t.deepEqual(ops, [
@@ -191,32 +191,32 @@ test('transform: updateOne', t => {
         filter: {
           $and: [
             { x: 1 },
-            { $or: [{ __v: { $lt: 2 } }, { __v: 2, __op: { $lt: 0 } }] },
-          ],
+            { $or: [{ __v: { $lt: 2 } }, { __v: 2, __op: { $lt: 0 } }] }
+          ]
         },
         update: {
           $set: { x: 2, __v: 2, __op: 0 },
-          $inc: { y: 1 },
+          $inc: { y: 1 }
         },
-        upsert: false,
-      },
-    },
+        upsert: false
+      }
+    }
   ]);
 });
 
 const sampleChildrenBornEventHandler = event => {
-  if (event.type === 'CHILDREN_BORN') {
+  if (event.type === "CHILDREN_BORN") {
     const {
       payload: { children, parents },
-      meta,
+      meta
     } = event;
     return [
       {
         insertMany: children.map(child => ({
           id: child.id,
           full_name: child.name,
-          born_at: meta.occurred_at,
-        })),
+          born_at: meta.occurred_at
+        }))
       },
       {
         updateMany: {
@@ -224,12 +224,12 @@ const sampleChildrenBornEventHandler = event => {
           changes: {
             $push: {
               children: {
-                $each: children.map(child => child.id),
-              },
-            },
-          },
-        },
-      },
+                $each: children.map(child => child.id)
+              }
+            }
+          }
+        }
+      }
     ];
   }
 
