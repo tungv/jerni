3598b2dd7a454148e871a14e07de845ca912decc
Tung Vu <me@tungv.com>
2018-08-27 17:58:13 +0700

    test(mongo): subscription should start after cleaning

--- packages/store-mongo/tests/e2e.test.js
+++ packages/store-mongo/tests/e2e.test.js
@@ -1,128 +1,94 @@
-const kefir = require('kefir');
-const test = require('ava');
+const kefir = require("kefir");
+const test = require("ava");
 
-const Model = require('../lib/MongoDBReadModel');
-const Connection = require('../lib/MongoDBConnection');
+const Model = require("../lib/MongoDBReadModel");
+const Connection = require("../lib/MongoDBConnection");
 
-const { MONGODB = 'mongodb://localhost:27017' } = process.env;
+const { MONGODB = "mongodb://localhost:27017" } = process.env;
 
 const makeStream = array => kefir.sequentially(1, array);
 
-test.cb('e2e', t => {
+test.cb("e2e", t => {
   const incomingBatchedEvents = [
-    [{ id: 1, type: 'TEST_1' }],
+    [{ id: 1, type: "TEST_1" }],
     [
-      { id: 2, type: 'TEST_2' },
-      { id: 3, type: 'TEST_3' },
-      { id: 4, type: 'TEST_1' },
+      { id: 2, type: "TEST_2" },
+      { id: 3, type: "TEST_3" },
+      { id: 4, type: "TEST_1" }
     ],
-    [{ id: 5, type: 'TEST_3' }, { id: 6, type: 'TEST_4' }],
-    [{ id: 7, type: 'TEST_5' }],
+    [{ id: 5, type: "TEST_3" }, { id: 6, type: "TEST_4" }],
+    [{ id: 7, type: "TEST_5" }]
   ];
   const stream = makeStream(incomingBatchedEvents);
 
   const model = new Model({
-    name: 'collection_1',
-    version: 'e2e',
+    name: "collection_1",
+    version: "e2e",
     transform: event =>
       [
         { insertOne: { event_type: event.type, counter: 0 } },
-        event.type === 'TEST_4' && {
+        event.type === "TEST_4" && {
           insertMany: [
             {
-              y: 1,
+              y: 1
             },
             {
-              y: 2,
+              y: 2
             },
             {
-              y: 3,
+              y: 3
             },
             {
-              y: 4,
-            },
-          ],
+              y: 4
+            }
+          ]
         },
         {
           updateOne: {
             where: {
-              event_type: 'TEST_1',
+              event_type: "TEST_1"
             },
             change: {
-              $inc: { counter: 1 },
-            },
-          },
+              $inc: { counter: 1 }
+            }
+          }
         },
         {
           updateMany: {
             where: {
-              counter: { $gte: 2 },
+              counter: { $gte: 2 }
             },
             changes: {
               $addToSet: {
-                array: { unique: event.id },
-              },
-            },
-          },
+                array: { unique: event.id }
+              }
+            }
+          }
         },
-        event.type === 'TEST_4' && {
+        event.type === "TEST_4" && {
           deleteOne: {
             where: {
-              event_type: 'TEST_3',
-            },
-          },
+              event_type: "TEST_3"
+            }
+          }
         },
-        event.type === 'TEST_5' && {
+        event.type === "TEST_5" && {
           deleteMany: {
             where: {
-              y: { $gte: 3 },
-            },
-          },
-        },
-      ].filter(x => x),
+              y: { $gte: 3 }
+            }
+          }
+        }
+      ].filter(x => x)
   });
 
   const conn = new Connection({
     url: MONGODB,
-    dbName: 'test_e2e',
-    models: [model],
+    dbName: "test_e2e",
+    models: [model]
   });
 
   let subscription;
-
-  conn.subscribe(async id => {
-    if (id === 7) {
-      const coll = await conn.getDriver(model);
-
-      const items = await coll.find({}).toArray();
-
-      t.deepEqual(items.map(({ _id, ...item }) => item), [
-        {
-          __op: 3,
-          __v: 7,
-          counter: 7,
-          event_type: 'TEST_1',
-          array: [
-            { unique: 2 },
-            { unique: 3 },
-            { unique: 4 },
-            { unique: 5 },
-            { unique: 6 },
-            { unique: 7 },
-          ],
-        },
-        { __op: 0, __v: 2, counter: 0, event_type: 'TEST_2' },
-        { __op: 0, __v: 4, counter: 0, event_type: 'TEST_1' },
-        { __op: 0, __v: 5, counter: 0, event_type: 'TEST_3' },
-        { __op: 0, __v: 6, counter: 0, event_type: 'TEST_4' },
-        { __op: 1, __v: 6, y: 1 },
-        { __op: 2, __v: 6, y: 2 },
-        { __op: 0, __v: 7, counter: 0, event_type: 'TEST_5' },
-      ]);
-      t.end();
-      subscription.unsubscribe();
-    }
-  });
 
   conn
     .clean()
@@ -130,6 +96,41 @@ test.cb('e2e', t => {
     .then(coll => coll.countDocuments({}))
     .then(count => {
       t.is(count, 0);
+
+      conn.subscribe(async id => {
+        if (id === 7) {
+          const coll = await conn.getDriver(model);
+
+          const items = await coll.find({}).toArray();
+
+          t.deepEqual(items.map(({ _id, ...item }) => item), [
+            {
+              __op: 3,
+              __v: 7,
+              counter: 7,
+              event_type: "TEST_1",
+              array: [
+                { unique: 2 },
+                { unique: 3 },
+                { unique: 4 },
+                { unique: 5 },
+                { unique: 6 },
+                { unique: 7 }
+              ]
+            },
+            { __op: 0, __v: 2, counter: 0, event_type: "TEST_2" },
+            { __op: 0, __v: 4, counter: 0, event_type: "TEST_1" },
+            { __op: 0, __v: 5, counter: 0, event_type: "TEST_3" },
+            { __op: 0, __v: 6, counter: 0, event_type: "TEST_4" },
+            { __op: 1, __v: 6, y: 1 },
+            { __op: 2, __v: 6, y: 2 },
+            { __op: 0, __v: 7, counter: 0, event_type: "TEST_5" }
+          ]);
+          t.end();
+          subscription.unsubscribe();
+        }
+      });
+
       conn.receive(stream).then(outputStream => {
         subscription = outputStream.observe();
       });
--- packages/store-mongo/tests/subscribe.test.js
+++ packages/store-mongo/tests/subscribe.test.js
@@ -1,51 +1,39 @@
-const kefir = require('kefir');
-const test = require('ava');
+const kefir = require("kefir");
+const test = require("ava");
 
-const Model = require('../lib/MongoDBReadModel');
-const Connection = require('../lib/MongoDBConnection');
+const Model = require("../lib/MongoDBReadModel");
+const Connection = require("../lib/MongoDBConnection");
 
 const makeStream = array => kefir.sequentially(10, array);
-const { MONGODB = 'mongodb://localhost:27017' } = process.env;
+const { MONGODB = "mongodb://localhost:27017" } = process.env;
 
-test.cb('subscribe', t => {
+test.cb("subscribe", t => {
   const incomingBatchedEvents = [
     [{ id: 1 }, { id: 2 }],
     [{ id: 3 }, { id: 4 }],
-    [{ id: 5 }, { id: 6 }],
+    [{ id: 5 }, { id: 6 }]
   ];
   const stream = makeStream(incomingBatchedEvents);
 
   const model1 = new Model({
-    name: 'collection_1',
-    version: 'abc',
-    transform: event => [{ insertOne: event }],
+    name: "collection_1",
+    version: "abc",
+    transform: event => [{ insertOne: event }]
   });
 
   const model2 = new Model({
-    name: 'collection_2',
-    version: '1.2.3',
-    transform: event => [{ insertOne: { x: 1 } }],
+    name: "collection_2",
+    version: "1.2.3",
+    transform: event => [{ insertOne: { x: 1 } }]
   });
 
   const conn = new Connection({
     url: MONGODB,
-    dbName: 'test_transform',
-    models: [model1, model2],
+    dbName: "test_transform",
+    models: [model1, model2]
   });
 
   let subscription;
-
-  conn.subscribe(async id => {
-    if (id === 6) {
-      const coll = await conn.getDriver(model1);
-
-      const items = await coll.find({}).toArray();
-      t.is(items.length, 6);
-      t.deepEqual(items.map(item => item.id), [1, 2, 3, 4, 5, 6]);
-      subscription.unsubscribe();
-      t.end();
-    }
-  });
 
   conn
     .clean()
@@ -53,6 +41,19 @@ test.cb('subscribe', t => {
     .then(coll => coll.countDocuments({}))
     .then(count => {
       t.is(count, 0);
+
+      conn.subscribe(async id => {
+        if (id === 6) {
+          const coll = await conn.getDriver(model1);
+
+          const items = await coll.find({}).toArray();
+          t.is(items.length, 6);
+          t.deepEqual(items.map(item => item.id), [1, 2, 3, 4, 5, 6]);
+          subscription.unsubscribe();
+          t.end();
+        }
+      });
+
       conn.receive(stream).then(outputStream => {
         subscription = outputStream.observe();
       });
