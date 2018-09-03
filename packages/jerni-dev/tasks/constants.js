ea143b27a77d0495ead5e50218c214f357b806ca
Tung Vu <me@tungv.com>
2018-09-03 09:52:37 +0700

    refactor(devtool): move DEV_DIR to `node_modules/.bin`
    this can avoid unnecessarily observable side-effects from other tools

--- packages/jerni-dev/tasks/constants.js
+++ packages/jerni-dev/tasks/constants.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const pkgDir = require("pkg-dir");
 
 exports.NAMESPACE = "local-dev";
-exports.DEV_DIR = path.resolve(process.cwd(), "./.jerni-dev");
+const rootDir = pkgDir.sync(process.cwd());
+exports.DEV_DIR = path.resolve(rootDir, "./node_modules/.bin/.jerni-dev");
 
 exports.getDevFile = f => path.resolve(exports.DEV_DIR, f);
