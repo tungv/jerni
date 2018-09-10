551743e33658e861e605fe53278d59d24209e2dd
Tung Vu <me@tungv.com>
2018-09-10 14:09:25 +0700

    fix(devtool): move DEV_DIR to User home

--- packages/jerni-dev/tasks/constants.js
+++ packages/jerni-dev/tasks/constants.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const pkgDir = require("pkg-dir");
-
+const home = require("home-or-tmp");
 exports.NAMESPACE = "local-dev";
 const rootDir = pkgDir.sync(process.cwd());
-exports.DEV_DIR = `~/.jerni-dev/${rootDir.split(/\W+/).join("_")}`;
+exports.DEV_DIR = `${home}/.jerni-dev/${rootDir.split(/\W+/).join("_")}`;
 
 exports.getDevFile = f => path.resolve(exports.DEV_DIR, f);
