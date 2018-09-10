c009271d9db195da25edd176db2b7af0b082781e
Tung Vu <me@tungv.com>
2018-09-10 11:47:49 +0700

    fix(devtool): exit 1 when inspect/clean fails

--- packages/jerni-dev/clean.js
+++ packages/jerni-dev/clean.js
@@ -6,20 +6,24 @@ const { DEV_DIR } = require("./tasks/constants");
 const createProxy = require("./lib/createProxy");
 
 module.exports = async filepath => {
-  const finalPath =
-    filepath[0] === "+"
-      ? require.resolve(filepath.slice(1))
-      : path.resolve(process.cwd(), filepath);
+  try {
+    console.log(`${colors.bgGreen.bold(" jerni-dev ")} clean`);
+    console.log("cwd:".padEnd(8), process.cwd());
+    console.log(
+      "DEV_DIR:".padEnd(8),
+      colors.italic.bold.blue(path.relative(process.cwd(), DEV_DIR))
+    );
 
-  console.log(`${colors.bgGreen.bold(" jerni-dev ")} clean`);
-  console.log("cwd:".padEnd(8), process.cwd());
-  console.log(
-    "DEV_DIR:".padEnd(8),
-    colors.italic.bold.blue(path.relative(process.cwd(), DEV_DIR))
-  );
-
-  const store = await createProxy(finalPath);
-  await store.DEV__cleanAll();
-  rimraf.sync(DEV_DIR);
-  store.destroy();
+    const finalPath =
+      filepath[0] === "+"
+        ? require.resolve(filepath.slice(1))
+        : path.resolve(process.cwd(), filepath);
+    const store = await createProxy(finalPath);
+    await store.DEV__cleanAll();
+    rimraf.sync(DEV_DIR);
+    store.destroy();
+  } catch (ex) {
+    console.error(ex);
+    process.exit(1);
+  }
 };
