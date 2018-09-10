a317b9cea07da72a4233c8b3f8a76752265c6989
Tung Vu <me@tungv.com>
2018-09-10 14:40:18 +0700

    refactor(devtool): don't need `home-or-dir`

--- packages/jerni-dev/tasks/constants.js
+++ packages/jerni-dev/tasks/constants.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const pkgDir = require("pkg-dir");
-const home = require("home-or-tmp");
+const os = require("os");
+const home = os.homedir() || os.tmpdir();
 exports.NAMESPACE = "local-dev";
 const rootDir = pkgDir.sync(process.cwd());
 exports.DEV_DIR = `${home}/.jerni-dev/${rootDir.split(/\W+/).join("_")}`;
