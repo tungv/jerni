2e3a575b2ba8e17d128765fb9101d9edd9d4ac28
Tung Vu <me@tungv.com>
2018-09-10 14:43:11 +0700

    refactor(devtool): keep directory structure for .jerni-dev

--- packages/jerni-dev/tasks/constants.js
+++ packages/jerni-dev/tasks/constants.js
@@ -4,6 +4,6 @@ const os = require("os");
 const home = os.homedir() || os.tmpdir();
 exports.NAMESPACE = "local-dev";
 const rootDir = pkgDir.sync(process.cwd());
-exports.DEV_DIR = `${home}/.jerni-dev/${rootDir.split(/\W+/).join("_")}`;
+exports.DEV_DIR = `${home}/.jerni-dev/${rootDir}`;
 
 exports.getDevFile = f => path.resolve(exports.DEV_DIR, f);
