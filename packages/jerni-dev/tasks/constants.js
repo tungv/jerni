030046e8e2cb552e2430c25fa5cabb65291bcdc4
Tung Vu <me@tungv.com>
2018-09-10 13:19:53 +0700

    refactor(devtool): move DEV_DIR to ~/.jerni-dev/<rootdir>

--- packages/jerni-dev/tasks/constants.js
+++ packages/jerni-dev/tasks/constants.js
@@ -3,6 +3,6 @@ const pkgDir = require("pkg-dir");
 
 exports.NAMESPACE = "local-dev";
 const rootDir = pkgDir.sync(process.cwd());
-exports.DEV_DIR = path.resolve(rootDir, "./node_modules/.bin/.jerni-dev");
+exports.DEV_DIR = `~/.jerni-dev/${rootDir.split(/\W+/).join("_")}`;
 
 exports.getDevFile = f => path.resolve(exports.DEV_DIR, f);
--- packages/jerni-dev/tasks/load-queue.js
+++ packages/jerni-dev/tasks/load-queue.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const fs = require("fs");
+const mkdirp = require("mkdirp");
 
 const { NAMESPACE, DEV_DIR } = require("./constants");
 
 module.exports = async () => {
   if (!fs.existsSync(DEV_DIR)) {
-    fs.mkdirSync(DEV_DIR);
+    mkdirp.sync(DEV_DIR);
   }
   const adapter = require("@heq/server-lokijs");
   return adapter({
