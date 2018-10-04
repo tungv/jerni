f262449d73f3f9e820dbb93ca7f3d55c184d05f1
Tung Vu <me@tungv.com>
2018-10-04 18:40:58 +0700

    feat(jerni): add --http option to change monitor port

--- packages/jerni/cli/index.js
+++ packages/jerni/cli/index.js
@@ -12,6 +12,7 @@ program.version(version);
 program
   .command("start <path>", "start a new journey", { default: true })
   .option("interval", "logging interval", 5 * 60 * 1000)
+  .option("http", "status reporting server port", 6181)
   .action((path, opts) => {
     process.env.NODE_ENV = "production";
     require("./jerni-start")(path, opts);
