2ef8fc21aab5f1af3ee9e7df8b6476834f9ea27a
Tung Vu <me@tungv.com>
2018-10-04 18:40:42 +0700

    fix(jerni): interval has to be a number instead of an object

--- packages/jerni/cli/index.js
+++ packages/jerni/cli/index.js
@@ -11,7 +11,7 @@ program.version(version);
 
 program
   .command("start <path>", "start a new journey", { default: true })
-  .option("interval", "logging interval", { default: 5 * 60 * 1000 })
+  .option("interval", "logging interval", 5 * 60 * 1000)
   .action((path, opts) => {
     process.env.NODE_ENV = "production";
     require("./jerni-start")(path, opts);
