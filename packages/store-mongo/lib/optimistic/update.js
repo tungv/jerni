e52cd002faefe733538b7db6a946c994e263dbdb
Tung Vu <me@tungv.com>
2019-01-24 18:57:37 +0700

    feat(mongo): allow arrayFilters

--- packages/store-mongo/lib/optimistic/update.js
+++ packages/store-mongo/lib/optimistic/update.js
@@ -1,25 +1,29 @@
-const returnEmptyOnException = require('../returnEmptyOnException');
+const returnEmptyOnException = require("../returnEmptyOnException");
 
 exports.applyUpdateMany = returnEmptyOnException((op, eventId, opId) => {
   return {
     ops: [{ updateMany: rawUpdate(op.updateMany, eventId, opId) }],
-    nextOpId: opId + 1,
+    nextOpId: opId + 1
   };
 });
 
 exports.applyUpdateOne = returnEmptyOnException((op, eventId, opId) => {
   return {
     ops: [{ updateOne: rawUpdate(op.updateOne, eventId, opId) }],
-    nextOpId: opId + 1,
+    nextOpId: opId + 1
   };
 });
 
-const rawUpdate = ({ changes, change, where }, eventId, opId) => {
+const rawUpdate = (
+  { changes, change, where, upsert = false, arrayFilters },
+  eventId,
+  opId
+) => {
   const { $set = {}, ...others } = { ...(change || changes) };
 
   const optimistic$set = Object.assign({}, $set, {
     __v: eventId,
-    __op: opId,
+    __op: opId
   });
 
   const update = Object.assign({ $set: optimistic$set }, others);
@@ -28,10 +32,13 @@ const rawUpdate = ({ changes, change, where }, eventId, opId) => {
     $and: [
       where,
       {
-        $or: [{ __v: { $lt: eventId } }, { __v: eventId, __op: { $lt: opId } }],
-      },
-    ],
+        $or: [{ __v: { $lt: eventId } }, { __v: eventId, __op: { $lt: opId } }]
+      }
+    ]
   };
 
-  return { filter, update, upsert: false };
+  if (arrayFilters) {
+    return { filter, update, upsert, arrayFilters };
+  }
+  return { filter, update, upsert };
 };
