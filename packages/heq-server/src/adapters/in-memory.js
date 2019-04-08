fffe56f2b273ae97e46347b09dadcb5d9d66047b
Tung <tung@bodidata.com>
2019-04-08 17:13:34 +0700

    refactor(server): switch to async generator api

--- packages/heq-server/src/adapters/in-memory.js
+++ packages/heq-server/src/adapters/in-memory.js
@@ -1,35 +1,45 @@
-const mitt = require('mitt');
-const kefir = require('kefir');
-
-const adapter = () => {
-  const emitter = mitt();
+function adapter() {
   const events = [];
   let id = 0;
 
   const commit = event => {
     events[id] = event;
     event.id = ++id;
-    // console.log('commit', id);
-    emitter.emit('data', event);
     return event;
   };
 
   const query = ({ from, to = id }) => {
-    // console.log('query', { from, to });
     return events.slice(from, to);
   };
 
-  const subscribe = () => {
-    // console.log('subscribe', { id });
-    const events$ = kefir.fromEvents(emitter, 'data');
-
-    return { events$ };
-  };
-
   const getLatest = () =>
-    events.length === 0 ? { id: 0, type: '@@INIT' } : events[events.length - 1];
+    events.length === 0 ? { id: 0, type: "@@INIT" } : events[events.length - 1];
 
-  return { commit, subscribe, query, getLatest };
-};
+  async function* generate(from, max, time, filter) {
+    const buffer = [];
+    let i = from;
+
+    while (true) {
+      await sleep(time);
+      for (; i < events.length; ++i) {
+        const event = events[i];
+        if (filter(event)) {
+          buffer.push(event);
+
+          if (buffer.length >= max) {
+            yield buffer;
+            buffer.length = 0;
+          }
+        }
+      }
+      yield buffer;
+      buffer.length = 0;
+    }
+  }
+
+  return { commit, query, getLatest, generate };
+}
 
 module.exports = adapter;
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
