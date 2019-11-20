9fa6f476a9231ff1c8a10167a1e3c9b24cc946b9
Tung <tung@bodidata.com>
2019-11-20 14:57:27 +0700

    test: skip EACCES and add EADDRINUSE

--- packages/heq/tests/cli.test.js
+++ packages/heq/tests/cli.test.js
@@ -1,52 +1,71 @@
-const test = require('ava');
-const { start } = require('./utils');
+const test = require("ava");
+const { start } = require("./utils");
 
-test('should start normally', async t => {
+test("should start normally", async t => {
   const output = await start();
 
-  t.true(output.includes('8080'), 'must include port in INFO log');
+  t.true(output.includes("8080"), "must include port in INFO log");
   t.true(
-    output.includes('local - redis://localhost:6379'),
-    'must include redis in INFO log'
+    output.includes("local - redis://localhost:6379"),
+    "must include redis in INFO log",
   );
-  t.true(output.includes('terminating'), 'must include terminating at the end');
+  t.true(output.includes("terminating"), "must include terminating at the end");
 });
 
-test('should start with less than 300ms', async t => {
+test("should start with less than 300ms", async t => {
   const startTime = Date.now();
-  const output = await start({ args: ['--port=8081'], timeoutMs: 0 });
+  const output = await start({ args: ["--port=8081"], timeoutMs: 0 });
   t.true(Date.now() - startTime < 300);
 });
 
-test('should report if redis cannot be connected', async t => {
+test("should report if redis cannot be connected", async t => {
   const error = await t.throws(
     start({
-      args: ['--port=8082', '--redis=redis://localhost:9999'],
-    })
+      args: ["--port=8082", "--redis=redis://localhost:9999"],
+    }),
   );
 
-  t.true(error.message.includes('Redis connection to localhost:9999 failed'));
+  t.true(error.message.includes("Redis connection to localhost:9999 failed"));
 });
 
-test('should report if port cannot be opened', async t => {
+// skip this because it's hard to reproduce on local machine
+test.skip("should report if port cannot be opened", async t => {
   const error = await t.throws(
     start({
-      args: ['--port=80'],
-    })
+      args: ["--port=1"],
+    }),
   );
 
-  t.true(error.message.includes('Error: listen EACCES 0.0.0.0:80'));
+  t.true(error.message.includes("Error: listen EACCES"));
+});
+test("should report if port is occupied", async t => {
+  const dummyServer = require("http").createServer((req, res) => {
+    res.end("OK");
+  });
+
+  try {
+    dummyServer.listen(32891);
+  } catch (ex) {}
+  const error = await t.throws(
+    start({
+      args: ["--port=32891"],
+    }),
+  );
+
+  dummyServer.close();
+
+  t.true(error.message.includes("Error: listen EADDRINUSE"));
 });
 
-test('should pickup env variables HEQ_*', async t => {
-  process.env.HEQ_PORT = '8083';
-  process.env.HEQ_REDIS_NAMESPACE = 'test-env';
-  process.env.HEQ_REDIS_URL = 'redis://localhost:6379/1';
+test("should pickup env variables HEQ_*", async t => {
+  process.env.HEQ_PORT = "8083";
+  process.env.HEQ_REDIS_NAMESPACE = "test-env";
+  process.env.HEQ_REDIS_URL = "redis://localhost:6379/1";
   const output = await start();
 
-  t.true(output.includes('8083'), 'must include port INFO log');
+  t.true(output.includes("8083"), "must include port INFO log");
   t.true(
-    output.includes('test-env - redis://localhost:6379/1'),
-    'must include redis in INFO log'
+    output.includes("test-env - redis://localhost:6379/1"),
+    "must include redis in INFO log",
   );
 });
