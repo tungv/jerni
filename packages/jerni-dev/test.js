9d66fc6c18e7e818eae1154af57cd0fbc0993ace
Tung <tung@bodidata.com>
2019-12-14 22:57:24 +0700

    feat(devtools): add journey.isPending

--- packages/jerni-dev/test.js
+++ packages/jerni-dev/test.js
@@ -69,6 +69,9 @@ module.exports = async function getJerniDevInstance(
         case "committed":
           return committed;
 
+        case "isPending":
+          return write > read;
+
         default:
           return target[property];
       }
