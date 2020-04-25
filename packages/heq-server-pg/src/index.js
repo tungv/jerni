5a4a2fca43c645e4b65f251fa7c55b457f53c57f
Tung <me@tungv.com>
2020-04-25 18:22:04 +0700

    feat: commit()

--- packages/heq-server-pg/src/index.js
+++ packages/heq-server-pg/src/index.js
@@ -1,13 +1,69 @@
-module.exports = function () {
+const { Pool } = require("pg");
+const uuid = require("@lukeed/uuid");
+
+module.exports = function ({ ns, connection }) {
+  const pool = new Pool(connection);
+
   const queue = { commit, generate, query, destroy, getLatest };
+
+  async function sql(query, params) {
+    const client = await pool.connect();
+    // TODO: set a name for better query planing on Postgres server
+    // @see https://node-postgres.com/features/queries
+    try {
+      return await client.query(query, params);
+    } catch (ex) {
+      console.error(ex);
+      throw ex;
+    } finally {
+      client.release();
+    }
+  }
 
   return queue;
 
   async function commit(event) {
-    return event;
+    const id = uuid();
+    const { type, payload = {}, meta = {} } = event;
+    const resp = await sql(
+      `SELECT write_message(
+        $1::varchar,
+        $2::varchar,
+        $3::varchar,
+        $4::jsonb,
+        $5::jsonb
+      );`,
+      [id, ns, type, payload, meta],
+    );
+
+    const {
+      rows: [{ write_message: idFromZero }],
+    } = resp;
+
+    return {
+      id: parseInt(idFromZero) + 1,
+      type,
+      payload,
+      meta,
+    };
   }
 
   async function query({ from = -1, to, types = [] }) {
     return [];
+  }
+
+  async function* generate() {
+    yield 1;
+  }
+
+  async function destroy() {
+    // tear down
+    console.time("destroyed");
+    await pool.end();
+    console.timeEnd("destroyed");
+  }
+
+  async function getLatest() {
+    return {};
   }
 };
--- packages/heq-server-pg/tests/queue.spec.js
+++ packages/heq-server-pg/tests/queue.spec.js
@@ -1,14 +1,37 @@
-const adapter = require("../src");
+const adapter = require("../src/index");
 
-test("it should commit", async () => {
+test("commit: id should go up", async () => {
+  const ns = `ns__${Math.random()}`;
   const queue = adapter({
-    ns: "test_commit",
-    host: "localhost",
-    port: "54320",
-    password: "simple",
+    ns,
+    connection: {
+      host: "localhost",
+      port: "54320",
+      password: "simple",
+      user: "message_store",
+      database: "message_store",
+    },
   });
 
-  const event = await queue.commit({ type: "test_1", payload: { a: "b" } });
-
-  expect(event).toEqual({ id: 1, type: "test_1", payload: { a: "b" } });
+  try {
+    const events = [];
+    for (let i = 0; i < 10; ++i) {
+      events.push(
+        await queue.commit({
+          type: "test",
+          payload: { key: i + 1 },
+        }),
+      );
+    }
+    expect(events).toEqual(
+      Array.from({ length: 10 }, (i, index) => ({
+        id: index + 1,
+        type: "test",
+        payload: { key: index + 1 },
+        meta: {},
+      })),
+    );
+  } finally {
+    await queue.destroy();
+  }
 });
