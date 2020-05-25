78df5adfa830f2e193e64d1680de1773f4d991ca
Tung <me@tungv.com>
2020-05-25 23:37:20 +0700

    fix: missing driver.session().run() proxying

--- packages/store-neo4j/src/createReadOnlyDriver.js
+++ packages/store-neo4j/src/createReadOnlyDriver.js
@@ -5,7 +5,7 @@ function trapTransaction(tx, ns) {
     get(target, prop, receiver) {
       if (prop === "run") {
         // console.log("[PROXY] transaction.run()");
-        return function(query, ...args) {
+        return function (query, ...args) {
           // console.log("[PROXY] query:", query, ...args);
           return target.run(ensureNamespace(query, ns), ...args);
         };
@@ -21,7 +21,7 @@ function trapSession(session, ns) {
     get(target, prop, receiver) {
       if (prop === "beginTransaction") {
         // console.log("[PROXY] session.%s()", prop);
-        return function(config) {
+        return function (config) {
           const tx = target.beginTransaction(config);
           return trapTransaction(tx, ns);
         };
@@ -29,16 +29,24 @@ function trapSession(session, ns) {
 
       if (prop === "readTransaction") {
         // console.log("[PROXY] session.%s()", prop);
-        return function(work, config) {
+        return function (work, config) {
           function trappedWork(tx) {
             return work.call(this, trapTransaction(tx, ns));
           }
 
           return target.readTransaction(trappedWork, config);
         };
-      } else {
-        return Reflect.get(target, prop, receiver);
       }
+
+      if (prop === "run") {
+        return function (query, ...args) {
+          // console.log("[PROXY] query:", query, ...args);
+          return target.run(ensureNamespace(query, ns), ...args);
+        };
+      }
+
+      // console.log("[PROXY] session.%s()", prop);
+      return Reflect.get(target, prop, receiver);
     },
   });
 }
@@ -48,7 +56,7 @@ module.exports = async function createReadOnlyDriver(driver, ns) {
     get(target, prop, receiver) {
       if (prop === "session") {
         // console.log("[PROXY] driver.%s()", prop);
-        return function(...args) {
+        return function (...args) {
           const session = target.session(...args);
 
           return trapSession(session, ns);
